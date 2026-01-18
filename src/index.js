e4750941bb9588056fabf1870bf5dd394996dc01
Tani <tanishkashah24@gmail.com>
2026-01-17 21:27:59 -0600

    Refresh deploy bundle

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './Main.css';
+import Main from './Main';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Main />
+  </React.StrictMode>
+);
